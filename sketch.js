eb8eac5890477bca4abd2bec5a68074c76e2ced7
lvyisgod <caylixx9@outlook.com>
2024-11-26 14:16:06 -0600

    added the tensorflow model to the html then trying to figure out how to load the model in

--- sketch.js
+++ sketch.js
@@ -4,7 +4,15 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let question = 'what gender is Bobo Fet';
+let passage = 'Bobo fet is a male';
 
+qna.load().then(model => {
+  // Find the answers
+  model.findAnswers(question, passage).then(answers => {
+    console.log('Answers: ', answers);
+  });
+});
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +20,5 @@ function setup() {
 
 function draw() {
   background(220);
-
-  circle(mouseX, mouseY, 100);
+  textAlign(CENTER, CENTER);
 }

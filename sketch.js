a6dc593016e6653493f3adc2232109a4e1fecf67
lvyisgod <caylixx9@outlook.com>
2024-12-04 14:16:15 -0600

    cleaning up my code
    Making my code look better by using css elements

--- sketch.js
+++ sketch.js
@@ -14,7 +14,9 @@ let userPassage;
 let userQuestion;
 let answerButton;
 let answer;
-let bg;
+let bg = "green";
+let state = "startScreen";
+let qnaHasLoaded = false;
 
 class qnaBot{
   constructor(){
@@ -33,56 +35,71 @@ class qnaBot{
 }
 
 function preload(){
-  bg = loadImage("robotMeme.jpg");
+  // bg = loadImage("robotMeme.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  newBot = new qnaBot();
-  newBot.loadQNAModel();
-
-  answerButton = createButton('click for answer');
-  answerButton.position(width/2, height/1.9)
-  answerButton.center("horizontal");
-  answerButton.mousePressed(() => {
-    question = userQuestion.elt.value;
-    passage = userPassage.elt.value;
-    newBot.findTheAnswer();
-  })
-
-  userPassage = createElement("textarea", "Input a passage");
-  userQuestion = createElement("textarea", "Input a question");
-  userPassage.size(width/2, height/3)
-  userQuestion.size(350, 50);
-  userPassage.position(0, height/16);
-  userQuestion.position(0, height/2.25); 
-  userPassage.center("horizontal");
-  userQuestion.center("horizontal");
 }
 
 function draw() {
-  background(bg);
-  textAlign(CENTER, CENTER);
-  textSize(15);
-  fill('white');
 
-
-  try {
-    if (newBot.answer.length <= 0){
-      text("Error no answer found", width/2, height/1.6);
-    }
-
-    for (let i = 0; i < newBot.answer.length; i++){
-      text(`Answer: ${newBot.answer[i].text}  confidence score: ${newBot.answer[i].score.toFixed(2)}`, width/2, height/1.6 + i * 20);
-    }
+  if (state === "startScreen"){
+    background("white");
   }
 
-  catch {
-    if (isModelLoaded){
-      text("model is loaded", width/2, height/1.6);
+  else if (state === "qnaScreen"){
+    if (!qnaHasLoaded){
+      newBot = new qnaBot();
+      newBot.loadQNAModel();
+
+      answerButton = createButton('click for answer');
+      answerButton.position(width/2, height/1.9);
+      answerButton.center("horizontal");
+      answerButton.mousePressed(() => {
+        question = userQuestion.elt.value;
+        passage = userPassage.elt.value;
+        newBot.findTheAnswer();
+      });
+
+      userPassage = createElement("textarea", "Input a passage");
+      userQuestion = createElement("textarea", "Input a question");
+      // userPassage.size(width/2, height/3);
+      userQuestion.size(350, 50);
+      // userPassage.position(0, height/16);
+      userQuestion.position(0, height/2.25); 
+      // userPassage.center("horizontal");
+      // userQuestion.center("horizontal");
+      qnaHasLoaded = true;
     }
-    else{
-      text("Please wait for model to load", width/2, height/1.6);
+
+    background(bg);
+    textAlign(CENTER, CENTER);
+    textSize(15);
+    fill('white');
+
+
+    try {
+      if (newBot.answer.length <= 0){
+        text("Error no answer found", width/2, height/1.6);
+      }
+
+      for (let i = 0; i < newBot.answer.length; i++){
+        text(`Answer: ${newBot.answer[i].text}  confidence score: ${newBot.answer[i].score.toFixed(2)}`, width/2, height/1.6 + i * 20);
+      }
+    }
+
+    catch {
+      if (isModelLoaded){
+        text("model is loaded", width/2, height/1.6);
+      }
+      else{
+        text("Please wait for model to load", width/2, height/1.6);
+      }
     }
   }
+}
+
+function mousePressed(){
+  state = "qnaScreen";
 }
